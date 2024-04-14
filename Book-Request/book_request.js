3a0a0ee179a05bd360a3401ea8387c66cef3e4cf
Matt <84277551+MattRueter@users.noreply.github.com>
2024-04-14 15:26:48 -0500

    rework flow of program to match new requirements.

--- Book-Request/book_request.js
+++ Book-Request/book_request.js
@@ -1,24 +1,32 @@
 const {requests, sampleRequests} = require("./requests");
 const { prioritizeGenres, selectBooksByGenre } = require("./utils/genres");
+const handleDuplicates = require("./utils/handleDuplicates");
 
 function createPurchaseList (requests, budget, genres) {
   if(requests.length <= 0){return null}
-  let purchaseList = [];
   
-  // First: get as many books as possible which belong to genres being prioritized. (genres)
-  const priority = prioritizeGenres(requests, genres);
-  
-  // Second: check 'priority' books are within budget. Get as many as possible and add them 
-  // to purchaseList.
-  purchaseList = selectBooksByGenre(priority, budget).map((book) => book);
+  //Author priority step:
+  // First: filter only books whose genre is a prioritized one.
+  // const authorPrioritzed(requests, authors);
+  // Second:  check books are within budget. Return as many as possible.
+  //const purchaseAuthors = selectBooksByAuthor(authorPrioritized, budget).map((book) => book);
+
+  //Genre priority step:
+  // First: filter only books whose genre is a prioritized one.
+  const genrePrioritized = prioritizeGenres(requests, genres);
+  // Second: Get rid of duplicates
+  const genreCleaned = handleDuplicates(genrePrioritized)
+  // Second: check books by are within budget. Return as many as possible.
+  const purchaseGenres = selectBooksByGenre(genreCleaned, budget).map((book) => book);
   
  
 
   //Next we need to add as many other requested books as budget allows. *consider optimizing for including from range of users?
   //Iterate over the rest of the books in request array and if the book doesn't exceed budget add it otherwise reject it.
   
-  //At this point the purchaseList will be complete.
-  return purchaseList;
+  //Combine all lists for the final purchaseList
+  //return purchaseList;
+  return purchaseGenres
 }
 
 module.exports = createPurchaseList;
